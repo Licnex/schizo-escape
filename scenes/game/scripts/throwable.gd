e57b94b71c74a157ee53a1b7cc18ce842ffd6aca
Vulpesvulpes828 <126353218+Godseeker-Yharim@users.noreply.github.com>
2026-08-29 22:55:51 -0500

    added a death and respawn system

--- scenes/game/scripts/throwable.gd
+++ scenes/game/scripts/throwable.gd
@@ -1,5 +1,17 @@
 extends RigidBody2D
+@onready var particles: CPUParticles2D = $CPUParticles2D
+@onready var sprite: Sprite2D = $Sprite2D
+
+
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body is not CharacterBody2D:
-		queue_free()
+		_hit_wall()
+
+
+func _hit_wall():
+	particles.emitting = true
+	sprite.visible = false
+
+func _on_cpu_particles_2d_finished() -> void:
+	queue_free()
